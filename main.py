2e5bd471808d11d8d05178edac1aad2eb86d0e2d
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-21 14:12:12 +0300

    Implement snake & apple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from settings import *
+from random import randint
 
 class Game(ctk.CTk):
     def __init__(self):
@@ -11,9 +12,31 @@ class Game(ctk.CTk):
         self.columnconfigure(list(range(FIELDS[0])), weight=1, uniform="a")
         self.rowconfigure(list(range(FIELDS[1])), weight=1, uniform="a")
 
+        self.snake = [START_POS,(START_POS[0]-1,START_POS[1]),(START_POS[0]-2,START_POS[1])]
 
-        self.grid(row=FIELDS)
+        self.place_apple()
+
+        self.draw_frames = []
+
+        self.draw()
 
         self.mainloop()
+
+    def place_apple(self):
+        self.apple_pos = (randint(0, FIELDS[0]-1),randint(0,FIELDS[1]-1))
+        
+
+    def draw(self):
+
+        apple_frame = ctk.CTkFrame(self, fg_color=APPLE_COLOR)
+        self.draw_frames.append((apple_frame, self.apple_pos))
+
+        for index, pos in enumerate(self.snake):
+            color = SNAKE_BODY_COLOR if index != 0 else SNAKE_HEAD_COLOR
+            snake_frame = ctk.CTkFrame(self, fg_color=color, corner_radius=0)
+            self.draw_frames.append((snake_frame, pos))
+
+        for frame, pos in self.draw_frames:
+            frame.grid(column=pos[0], row=pos[1])
 
 Game()
