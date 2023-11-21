6d383bc520e47e0e8c3c5a4795a0ca4b80eb6363
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-21 14:37:30 +0300

    Implement movement of snake

--- main.py
+++ main.py
@@ -13,20 +13,44 @@ class Game(ctk.CTk):
         self.rowconfigure(list(range(FIELDS[1])), weight=1, uniform="a")
 
         self.snake = [START_POS,(START_POS[0]-1,START_POS[1]),(START_POS[0]-2,START_POS[1])]
+        self.direction = DIRECTIONS["right"]
+        self.bind("<Key>", self.get_input)
 
         self.place_apple()
 
         self.draw_frames = []
 
-        self.draw()
+        self.animate()
 
         self.mainloop()
+
+    def get_input(self, event):
+        match event.keycode:
+            case 37: self.direction = DIRECTIONS["left"]
+            case 38: self.direction = DIRECTIONS["up"]
+            case 39: self.direction = DIRECTIONS["right"]
+            case 40: self.direction = DIRECTIONS["down"]
+        print(self.direction)
+
+    def animate(self):
+        new_head = (self.snake[0][0] + self.direction[0], self.snake[0][1] + self.direction[1])
+        self.snake.insert(0, new_head)
+
+        self.snake.pop()
+
+        self.draw()
+
+        self.after(250, self.animate)
 
     def place_apple(self):
         self.apple_pos = (randint(0, FIELDS[0]-1),randint(0,FIELDS[1]-1))
         
-
     def draw(self):
+        if self.draw_frames:
+            for frame, pos in self.draw_frames:
+                frame.grid_forget()
+            self.draw_frames.clear()
+
 
         apple_frame = ctk.CTkFrame(self, fg_color=APPLE_COLOR)
         self.draw_frames.append((apple_frame, self.apple_pos))
